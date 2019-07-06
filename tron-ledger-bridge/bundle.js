b74bbfc28eb4061765a205cc13c6544f8978d1c1
ZacharyLe <LeeZachary19890505@gmail.com>
2019-07-06 20:05:23 +0800

    test

--- tron-ledger-bridge/bundle.js
+++ tron-ledger-bridge/bundle.js
@@ -229,6 +229,7 @@ _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function 
 
 var checkTronWeb = setInterval(function () {
   var tronWeb = window.tronWeb;
+  console.log(tronWeb.defaultAddress);
 
   if (tronWeb && tronWeb.defaultAddress.base58) {
     clearInterval(checkTronWeb);
--- tron-ledger-bridge/index.js
+++ tron-ledger-bridge/index.js
@@ -48,6 +48,7 @@ import { delay } from './ledger/utils';
 })()
 var checkTronWeb = setInterval(()=>{
     const tronWeb = window.tronWeb;
+    console.log(tronWeb.defaultAddress);
     if(tronWeb && tronWeb.defaultAddress.base58){
         clearInterval(checkTronWeb);
         tronWeb.trx.sign = bridge.buildTransactionSigner(tronWeb);
