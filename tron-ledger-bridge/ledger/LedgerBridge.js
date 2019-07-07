446a0b7e4156da175b0b651f6130642939ff8624
ZacharyLe <LeeZachary19890505@gmail.com>
2019-07-07 12:41:41 +0800

    test

--- tron-ledger-bridge/ledger/LedgerBridge.js
+++ tron-ledger-bridge/ledger/LedgerBridge.js
@@ -8,7 +8,9 @@ import AppTrx from "./Tron";
 import Transport from "@ledgerhq/hw-transport-u2f";
 import {transactionJsonToProtoBuf} from "@tronscan/client/src/utils/tronWeb";
 import {byteArray2hexStr} from "@tronscan/client/src/utils/bytes";
-
+import { Client as ApiClient } from "@tronscan/client";
+const ledgerTokenList = require('./tokens');
+const Client = new ApiClient();
 //const baseUrl = 'https://metamask.github.io/eth-ledger-bridge-keyring';
 export default class LedgerBridge {
     constructor() {
@@ -103,6 +105,35 @@ export default class LedgerBridge {
         });
     }
 
+    async getTokenExtraInfo(ID){
+        let tokenID = ID;
+        if (typeof tokenID != "number") {
+            tokenID = tronWeb.toUtf8(tokenID);
+            if (tokenID==="_")
+                return {id: 0, decimals: 6, token_name: "TRX"};
+            else
+                tokenID = parseInt(tokenID);
+        }
+
+        const token = await Client.getTokens({id: tokenID});
+        if (token.total==1){
+            return {id: tokenID, decimals: token.tokens[0].precision, token_name: token.tokens[0].name};
+        }
+        return {id: -1, decimals: 0, token_name: ""};;
+    }
+
+    getLedgerTokenInfo(ID){
+        let tokenID = ID;
+        if (typeof tokenID != "number") {
+            if (tokenID==="_")
+                tokenID = 0;
+            else
+                tokenID = parseInt(tokenID);
+        }
+        return ledgerTokenList.tokenList.find(o => o.id === tokenID );
+    }
+
+
     buildTransactionSigner(tronWeb) {
         return async (transaction) => {
             try {
@@ -129,62 +160,8 @@ export default class LedgerBridge {
                             extra = await this.getTokenExtraInfo(transaction.raw_data.contract[0].parameter.value.asset_name);
                             tokenInfo.push(this.getLedgerTokenInfo(ID).message);
                             break;
-                        case 41: //ExchangeCreateContract
-                            const token1 =  await this.getTokenExtraInfo(
-                            transaction.raw_data.contract[0].parameter.value.first_token_id
-                        );
-                            const token2 =  await this.getTokenExtraInfo(
-                            transaction.raw_data.contract[0].parameter.value.second_token_id
-                        );
-                            if (token1!== undefined && token2!== undefined){
-                                extra = {
-                                    token1: token1.token_name,
-                                    decimals1: token1.decimals,
-                                    token2: token2.token_name,
-                                    decimals2: token2.decimals,
-                                }
-                                if (token1.id!=0)tokenInfo.push(this.getLedgerTokenInfo(token1.id).message);
-                                if (token2.id!=0)tokenInfo.push(this.getLedgerTokenInfo(token2.id).message);
-                            }
-                            break;
-                        case 42: //ExchangeInjectContract
-                            const exchangeDepositID = transaction.raw_data.contract[0].parameter.value.exchange_id;
-                            const exchangeDeposit = this.getLedgerExchangeInfo(exchangeDepositID);
-                            const exchangeDepositToken = this.getLedgerTokenInfo(tronWeb.toUtf8(
-                                transaction.raw_data.contract[0].parameter.value.token_id)
-                            );
-                            // get exchange info
-                            extra = {
-                                pair: exchangeDeposit.pair,
-                                token: exchangeDepositToken.token_name,
-                                decimals: exchangeDepositToken.decimals,
-                            };
-                            if (exchangeDepositToken.id!=0) tokenInfo.push(exchangeDepositToken.message);
-                            break;
-                        case 43: //ExchangeWithdrawContract
-                            const exchangeWithdrawID = transaction.raw_data.contract[0].parameter.value.exchange_id;
-                            const exchangeWithdraw = this.getLedgerExchangeInfo(exchangeWithdrawID);
-                            const exchangeWithdrawToken = this.getLedgerTokenInfo(tronWeb.toUtf8(
-                                transaction.raw_data.contract[0].parameter.value.token_id)
-                            );
-                            // get exchange info
-                            extra = {
-                                pair: exchangeWithdraw.pair,
-                                token: exchangeWithdrawToken.token_name,
-                                decimals: exchangeWithdrawToken.decimals,
-                            };
-                            if (exchangeWithdrawToken.id!=0) tokenInfo.push(exchangeWithdrawToken.message);
-                            break;
-                        case 44: //ExchangeTransactionContract
-                            const exchangeID = transaction.raw_data.contract[0].parameter.value.exchange_id;
-                            const exchange = this.getLedgerExchangeInfo(exchangeID);
-                            // get exchange info
-                            extra = {
-                                pair: exchange.pair, decimals1: exchange.decimals1, decimals2: exchange.decimals2,
-                                action: ((transaction.raw_data.contract[0].parameter.value.token_id===exchange.firstToken)?"Sell":"Buy"),
-                            };
-                            tokenInfo.push(exchange.message);
-                            break;
+
+
 
                         case 31: //Trigger Smart Contract
                             extra = transaction.extra;
